0772d397f9d4f3a35d5075b0006dc950a393a0b9
Elena <olpetrushova@gmail>
2017-12-05 11:38:12 +0200

    added filter left menu with a width of less than 1200 px

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
 $(function(){
 
-	var menu = document.getElementById('toggler');
-    var title = menu.querySelector('.li');
-
-    title.onclick = function() {
-      menu.classList.toggle('open');
-    };
-
+	
 	
 });
    
