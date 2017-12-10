bc12d6d9829ff75539e7395dc9381c8ab51b64cd
Elena <olpetrushova@gmail>
2017-12-10 20:50:15 +0200

    added owl-carousel

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 $(function(){
 
-	
+	$(".owl-carousel").owlCarousel({
+  	//loop:true,
+  	items:1,
+  	nav:true,
+  	navText:["<img src=\"icons/Next-Icon.png\">","<img src=\"icons/Next-Icon.png\">"],
+  	smartSpeed: 800,
+  })
 	
 });
    
