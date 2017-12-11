6a3bf5c23e58ac6a4d30337c6c1220fc0c89812f
Elena <olpetrushova@gmail>
2017-12-11 21:35:04 +0200

    added pagination and best sellers

--- js/main.js
+++ js/main.js
@@ -6,6 +6,18 @@ $(function(){
   	nav:true,
   	navText:["<img src=\"icons/Next-Icon.png\">","<img src=\"icons/Next-Icon.png\">"],
   	smartSpeed: 800,
+  	responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:1,
+        },
+        1000:{
+            items:1,
+        }
+    }
   })
 	
 });
